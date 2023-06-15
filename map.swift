dfe1fcbb8fb3937428f20008d75d4d3610de3e4b
Berkinkaya <berkinkaya-99@hotmail.com>
2023-06-15 23:40:24 +0300

    Final form

---
+++ map.swift
@@ -0,0 +1,20 @@
+//
+//  map.swift
+//  SwitftUITutorial
+//
+//  Created by Berkin Kaya on 15.06.2023.
+//
+
+import SwiftUI
+
+struct map: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct map_Previews: PreviewProvider {
+    static var previews: some View {
+        map()
+    }
+}
---
+++ turtleRockImage.swift
@@ -0,0 +1,20 @@
+//
+//  turtleRockImage.swift
+//  SwitftUITutorial
+//
+//  Created by Berkin Kaya on 15.06.2023.
+//
+
+import SwiftUI
+
+struct turtleRockImage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct turtleRockImage_Previews: PreviewProvider {
+    static var previews: some View {
+        turtleRockImage()
+    }
+}
