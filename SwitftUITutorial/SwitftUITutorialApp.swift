d317dfb5d0ac419980e95a0322d9d5c4727f7c92
Berkinkaya <berkinkaya-99@hotmail.com>
2023-06-15 23:06:03 +0300

    Initial Commit

---
+++ SwitftUITutorial/SwitftUITutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwitftUITutorialApp.swift
+//  SwitftUITutorial
+//
+//  Created by Berkin Kaya on 15.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwitftUITutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
